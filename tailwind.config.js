d103c854b22e684352cba2b6cb3f062a771904fa
RamanShrikant <ramanshrikant2@gmail.com>
2025-09-02 01:06:54 -0400

    Add files via upload
    New updates :
    Switched from Bulma CSS to Tailwind CSS for greater control and to help transition this project to implement React in the next update
    Added a logo
    Other UI and design changes (team names and logos are better centered and are more visually appealing)

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ["./popup.html", "./popup.js"], // scan these files for class names
+  theme: { extend: {} },
+  corePlugins: { preflight: false },
+  plugins: [],
+};
