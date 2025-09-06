ceacb7a5689cd71fbb3afbf6ed1d3d2baed61651
RamanShrikant <ramanshrikant2@gmail.com>
2025-09-05 20:40:32 -0400

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  content: ["./popup.html", "./popup.js"], // scan these files for class names
-  theme: { extend: {} },
-  corePlugins: { preflight: false },
-  plugins: [],
-};
